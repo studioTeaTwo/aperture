3f972eea4e48f7eec8c758a4f32338fc1ad6499f
T2 <studioTeaTwo@gmail.com>
2023-12-12 11:19:03 +0900

    refactor(nostr): add time zone

--- nostr/nostr.go
+++ nostr/nostr.go
@@ -86,7 +86,7 @@ func (n *NostrClient) PublishEvent(p *NostrPublishParam) error {
 	// note: message is expected to be the user's receipt
 	message := n.servicename +
 		" article=" + p.Slug +
-		" settleDate=" + time.Unix(p.Invoice.GetSettleDate(), 0).Format("2006-01-02T15:04:05") +
+		" settleDate=" + time.Unix(p.Invoice.GetSettleDate(), 0).Format("2006-01-02T15:04:05MST") +
 		" price=" + strconv.FormatInt(p.Price*1000, 10) +
 		" paidAmount=" + strconv.FormatInt(p.Invoice.GetAmtPaidMsat(), 10) +
 		" preimage=" + preimage.String() +
